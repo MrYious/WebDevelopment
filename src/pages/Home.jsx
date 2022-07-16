b06636fb3088daaf280251c13cf041943cf8d3fb
MrYious <rosariomark37@gmail.com>
2022-07-17 00:44:48 +0800

    feat: fixed Login System with Database

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,32 +9,32 @@ const links = [
 
 const Home = () => {
   return (<>
-    <div className='flex flex-col bg-slate-100 h-screen'>
+    <div className='flex flex-col h-screen bg-slate-100'>
       {/* NAVBAR */}
-      <div className="flex w-full h-16 px-8 py-3 border-black border-b-2 bg-indigo-300">
+      <div className="flex w-full h-16 px-8 py-3 bg-indigo-300 border-b-2 border-black">
         {/* ICON */}
-        <div className="flex items-center font-bold w-1/6">
+        <div className="flex items-center w-1/6 font-bold">
           Quiz Makers
         </div>
         {/* LINKS */}
-        <div className="flex justify-between gap-4 h-full w-5/6 ">
+        <div className="flex justify-between w-5/6 h-full gap-4 ">
           {/* SCROLL */}
           <div className="flex gap-8">
             {links.map((link, i) => {
               return <Link to='' key={i}>
-                <button className="h-full px-3 rounded-3xl font-bold">{link.name}</button>
+                <button className="h-full px-3 font-bold rounded-3xl">{link.name}</button>
               </Link>
             })}
           </div>
           {/* ACCOUNT */}
           <div>
             <Link to='user/signup'>
-              <button className="h-full px-4 py-1 rounded-3xl font-bold">
+              <button className="h-full px-4 py-1 font-bold rounded-3xl">
                 Register
               </button>
             </Link>
             <Link to='user/'>
-              <button className="h-full px-4 py-1 border-black border-2 rounded-3xl font-bold">
+              <button className="h-full px-4 py-1 font-bold border-2 border-black rounded-3xl">
                 Sign In
               </button>
             </Link>
